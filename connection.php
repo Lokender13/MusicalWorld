78036f488e3aeee7069aa42006fd2dcb966ad591
Lokender Chauhan <101618188+Lokender13@users.noreply.github.com>
2022-04-09 09:33:58 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+	$conn = mysqli_connect('sql671.main-hosting.eu','u429173902_musical','Musical@1122','u429173902_musical');
+
+	if(!$conn)
+		die("Error while connecting...!").mysqli_connect_error($conn);
+
+ ?>
